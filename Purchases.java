e591144b91a09813757fd10e22eb4df4529ca671
racheledler <55890230+racheledler@users.noreply.github.com>
2020-12-01 19:18:14 -0500

    Create Purchases.java

---
+++ Purchases.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Purchases {
+
+    private String purchase_Product;
+    private double purchase_Price;
+
+
+    public String getPurchase_Product() {
+        return purchase_Product;
+    }
+
+    public void setPurchase_Product(String purchase_Product) {
+        this.purchase_Product = purchase_Product;
+    }
+
+    public double getPurchase_Price() {
+        return purchase_Price;
+    }
+
+    public void setPurchase_Price(double purchase_Price) {
+        this.purchase_Price = purchase_Price;
+    }
+}
