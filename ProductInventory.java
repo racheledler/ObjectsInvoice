3587e04b128c9b116acb0f2da676796edb64ae71
racheledler <55890230+racheledler@users.noreply.github.com>
2020-12-01 19:17:50 -0500

    Create ProductInventory.java

---
+++ ProductInventory.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class ProductInventory extends Purchases {
+
+    private String product;
+    private Double price;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+}
